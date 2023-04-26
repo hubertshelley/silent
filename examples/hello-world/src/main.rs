d461681896d555ca7cef22a6ad21a676fae27c0e
hubertshelley <hubertshelley@163.com>
2023-04-26 13:36:23 +0800

    路由基础匹配已完成

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -4,14 +4,30 @@ fn main() {
     logger::fmt::init();
     let route = Route {
         path: "".to_string(),
-        handler: None,
+        handler: Some("hello world".to_string()),
         children: vec![Route {
             path: "1".to_string(),
-            handler: None,
-            children: vec![],
+            handler: Some("hello world1".to_string()),
+            children: vec![
+                Route {
+                    path: "1".to_string(),
+                    handler: Some("hello world11".to_string()),
+                    children: vec![],
+                    middlewares: vec![],
+                },
+                Route {
+                    path: "2".to_string(),
+                    handler: Some("hello world12".to_string()),
+                    children: vec![],
+                    middlewares: vec![],
+                },
+            ],
             middlewares: vec![],
         }],
         middlewares: vec![],
     };
-    Server::new().bind_route(route).run();
+    Server::new()
+        .bind("127.0.0.1:8001".parse().unwrap())
+        .bind_route(route)
+        .run();
 }
--- silent/src/route/mod.rs
+++ silent/src/route/mod.rs
@@ -15,25 +15,29 @@ pub struct Route {
 
 impl Match for Route {
     fn handler_match(&self, path: &str) -> Matched {
-        for route in &self.children {
-            if let Matched::Matched(route) = route.handler_match(path) {
-                return Matched::Matched(route);
+        let (local_url, last_url) = Self::path_split(path);
+        if self.path == local_url {
+            if last_url.is_empty() {
+                return Matched::Matched(self.clone());
+            } else {
+                for route in &self.children {
+                    if let Matched::Matched(route) = route.handler_match(last_url) {
+                        return Matched::Matched(route);
+                    }
+                }
             }
-        }
-        if self.path == path {
-            return Matched::Matched(self.clone());
         }
         Matched::Unmatched
     }
 }
 
 impl Route {
-    // fn path_split(path: &str) -> (&str, &str) {
-    //     let mut iter = path.splitn(2, '/');
-    //     let first = iter.next().unwrap_or("");
-    //     let second = iter.next().unwrap_or("");
-    //     (first, second)
-    // }
+    fn path_split(path: &str) -> (&str, &str) {
+        let mut iter = path.splitn(2, '/');
+        let local_url = iter.next().unwrap_or("");
+        let last_url = iter.next().unwrap_or("");
+        (local_url, last_url)
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -65,8 +69,11 @@ impl Routes {
         println!("{:?}", req);
         match self.handler_match(req.uri().path()) {
             Matched::Matched(route) => {
+                if route.handler.is_none() {
+                    return Err((String::from("404"), StatusCode::NOT_FOUND));
+                }
                 println!("{:?}", route.middlewares);
-                Ok(Response::from("Hello World"))
+                Ok(Response::from(route.handler.unwrap()))
             }
             Matched::Unmatched => Err((String::from("404"), StatusCode::NOT_FOUND)),
         }
