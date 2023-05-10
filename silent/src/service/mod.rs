3a9e04ca3a9846adda5898234f2a9f4e5b6561fe
hubertshelley <hubertshelley@163.com>
2023-05-10 21:19:18 +0800

    py拓展编写中

--- silent/src/service/mod.rs
+++ silent/src/service/mod.rs
@@ -7,6 +7,7 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::net::TcpListener;
 use tokio::runtime::Runtime;
+use tokio::sync::mpsc::unbounded_channel;
 use tokio::sync::RwLock;
 
 pub struct Server {
@@ -49,20 +50,33 @@ impl Server {
         let Self { conn, routes, .. } = self;
         tracing::info!("Listening on http://{}", self.addr);
         let listener = TcpListener::bind(self.addr).await.unwrap();
+        // 捕获SIGINT信号
+        let (tx, mut rx) = unbounded_channel();
+
+        ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+            .expect("Error setting Ctrl-C handler");
         loop {
-            match listener.accept().await {
-                Ok((stream, _)) => {
-                    tracing::debug!("Accepting from: {}", stream.peer_addr().unwrap());
-                    let routes = routes.read().await.clone();
-                    let conn = conn.clone();
-                    tokio::task::spawn(async move {
-                        if let Err(err) = Serve::new(routes, conn).call(stream).await {
-                            tracing::error!("Failed to serve connection: {:?}", err);
-                        }
-                    });
+            tokio::select! {
+                _ = rx.recv() => {
+                    tracing::info!("Received Ctrl-C, shutting down");
+                    break;
                 }
-                Err(e) => {
-                    tracing::error!(error = ?e, "accept connection failed");
+                s = listener.accept() =>{
+                    match s{
+                        Ok((stream, _)) => {
+                            tracing::debug!("Accepting from: {}", stream.peer_addr().unwrap());
+                            let routes = routes.read().await.clone();
+                            let conn = conn.clone();
+                            tokio::task::spawn(async move {
+                                if let Err(err) = Serve::new(routes, conn).call(stream).await {
+                                    tracing::error!("Failed to serve connection: {:?}", err);
+                                }
+                            });
+                        }
+                        Err(e) => {
+                            tracing::error!(error = ?e, "accept connection failed");
+                        }
+                    }
                 }
             }
         }
