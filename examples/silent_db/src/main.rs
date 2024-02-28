da594be67331cea4efcec9867f0188fdf477c834
Hubert Shelley <46239302+hubertshelley@users.noreply.github.com>
2024-02-28 17:01:49 +0800

    Update main.rs

--- examples/silent_db/src/main.rs
+++ examples/silent_db/src/main.rs
@@ -7,7 +7,6 @@ async fn main() {
     let migrate = silent_db::Migrate::new(
         "migrations".to_string(),
         "mysql://root:123456@192.168.110.16:3306/test"
-            // "mysql://hubert22:wangdaofeng@rm-bp1dt1ar4q08xwvz15o.mysql.rds.aliyuncs.com:3306/test"
             .parse()
             .unwrap(),
     );
