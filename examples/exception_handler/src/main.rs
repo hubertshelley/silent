dbeef2de28d8b1d07b2c55c319509d7f1573dde3
hubertshelley <hubertshelley@163.com>
2024-08-07 08:32:36 +0800

    refactor: exception_handler重构

--- examples/exception_handler/src/main.rs
+++ examples/exception_handler/src/main.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+
+use silent::middlewares::ExceptionHandler;
 use silent::prelude::*;
 
 #[derive(Deserialize, Serialize, Debug)]
@@ -10,6 +12,7 @@ struct Exception {
 fn main() {
     logger::fmt().with_max_level(Level::INFO).init();
     let route = Route::new("")
+        .root_hook(ExceptionHandler::new(|res, _| async move { res }))
         .get(|mut req| async move { req.params_parse::<Exception>() })
         .route();
     Server::new().run(route);
--- silent/src/middleware/middlewares/mod.rs
+++ silent/src/middleware/middlewares/mod.rs
@@ -1,5 +1,7 @@
 mod cors;
+mod exception_handler;
 mod request_time_logger;
 
 pub use cors::{Cors, CorsType};
+pub use exception_handler::ExceptionHandler;
 pub use request_time_logger::RequestTimeLogger;
