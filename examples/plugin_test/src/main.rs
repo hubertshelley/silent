0c0e680d8bf72553f3e703a674f9cf4493a7f442
hubertshelley <hubertshelley@163.com>
2024-09-05 17:15:19 +0800

    feat[plugin]: 添加plugin示例

---
+++ examples/plugin_test/src/main.rs
@@ -0,0 +1,21 @@
+use crossbeam_epoch::Pointable;
+use dlopen2::wrapper::{Container, WrapperApi};
+use silent::prelude::{logger, Level, Route, Server};
+use std::error::Error;
+
+#[derive(WrapperApi)]
+struct PluginApi {
+    // plugin_metadata: unsafe extern "C" fn() -> PluginMetadata,
+    #[allow(improper_ctypes_definitions)]
+    get_route: unsafe extern "C" fn() -> Route,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    logger::fmt().with_max_level(Level::INFO).init();
+    let lib_path = "./libexamples_plugins.dylib";
+    let container: Container<PluginApi> =
+        unsafe { Container::load(lib_path) }.expect("无法加载插件");
+    let route = unsafe { container.get_route() };
+    Server::new().run(route);
+    Ok(())
+}
