f56716920e21c07e5ab09d3613cb8ac3ca7c8b56
hubertshelley <hubertshelley@163.com>
2024-09-05 17:15:19 +0800

    feat[plugin]: 添加plugin示例

---
+++ silent/src/ws/route.rs
@@ -0,0 +1,115 @@
+use crate::prelude::{HandlerGetter, Message, Result, WebSocketParts};
+use crate::route::Route;
+use crate::ws::{HandlerWrapperWebSocket, WebSocketHandler};
+use http::Method;
+use std::future::Future;
+use std::sync::Arc;
+use tokio::sync::mpsc::UnboundedSender;
+use tokio::sync::RwLock;
+use tokio_tungstenite::tungstenite::protocol::WebSocketConfig;
+
+pub trait WSHandlerAppend<
+    FnOnConnect,
+    FnOnConnectFut,
+    FnOnSend,
+    FnOnSendFut,
+    FnOnReceive,
+    FnOnReceiveFut,
+    FnOnClose,
+    FnOnCloseFut,
+>: HandlerGetter where
+    FnOnConnect: Fn(Arc<RwLock<WebSocketParts>>, UnboundedSender<Message>) -> FnOnConnectFut
+        + Send
+        + Sync
+        + 'static,
+    FnOnConnectFut: Future<Output = Result<()>> + Send + Sync + 'static,
+    FnOnSend: Fn(Message, Arc<RwLock<WebSocketParts>>) -> FnOnSendFut + Send + Sync + 'static,
+    FnOnSendFut: Future<Output = Result<Message>> + Send + Sync + 'static,
+    FnOnReceive: Fn(Message, Arc<RwLock<WebSocketParts>>) -> FnOnReceiveFut + Send + Sync + 'static,
+    FnOnReceiveFut: Future<Output = Result<()>> + Send + Sync + 'static,
+    FnOnClose: Fn(Arc<RwLock<WebSocketParts>>) -> FnOnCloseFut + Send + Sync + 'static,
+    FnOnCloseFut: Future<Output = ()> + Send + Sync + 'static,
+{
+    fn ws(
+        self,
+        config: Option<WebSocketConfig>,
+        handler: WebSocketHandler<
+            FnOnConnect,
+            FnOnConnectFut,
+            FnOnSend,
+            FnOnSendFut,
+            FnOnReceive,
+            FnOnReceiveFut,
+            FnOnClose,
+            FnOnCloseFut,
+        >,
+    ) -> Self;
+    fn ws_handler_append(
+        &mut self,
+        handler: HandlerWrapperWebSocket<
+            FnOnConnect,
+            FnOnConnectFut,
+            FnOnSend,
+            FnOnSendFut,
+            FnOnReceive,
+            FnOnReceiveFut,
+            FnOnClose,
+            FnOnCloseFut,
+        >,
+    ) {
+        let handler = Arc::new(handler);
+        self.get_handler_mut().insert(Method::GET, handler);
+    }
+}
+impl<
+        FnOnConnect,
+        FnOnConnectFut,
+        FnOnSend,
+        FnOnSendFut,
+        FnOnReceive,
+        FnOnReceiveFut,
+        FnOnClose,
+        FnOnCloseFut,
+    >
+    WSHandlerAppend<
+        FnOnConnect,
+        FnOnConnectFut,
+        FnOnSend,
+        FnOnSendFut,
+        FnOnReceive,
+        FnOnReceiveFut,
+        FnOnClose,
+        FnOnCloseFut,
+    > for Route
+where
+    FnOnConnect: Fn(Arc<RwLock<WebSocketParts>>, UnboundedSender<Message>) -> FnOnConnectFut
+        + Send
+        + Sync
+        + 'static,
+    FnOnConnectFut: Future<Output = Result<()>> + Send + Sync + 'static,
+    FnOnSend: Fn(Message, Arc<RwLock<WebSocketParts>>) -> FnOnSendFut + Send + Sync + 'static,
+    FnOnSendFut: Future<Output = Result<Message>> + Send + Sync + 'static,
+    FnOnReceive: Fn(Message, Arc<RwLock<WebSocketParts>>) -> FnOnReceiveFut + Send + Sync + 'static,
+    FnOnReceiveFut: Future<Output = Result<()>> + Send + Sync + 'static,
+    FnOnClose: Fn(Arc<RwLock<WebSocketParts>>) -> FnOnCloseFut + Send + Sync + 'static,
+    FnOnCloseFut: Future<Output = ()> + Send + Sync + 'static,
+{
+    fn ws(
+        mut self,
+        config: Option<WebSocketConfig>,
+        handler: WebSocketHandler<
+            FnOnConnect,
+            FnOnConnectFut,
+            FnOnSend,
+            FnOnSendFut,
+            FnOnReceive,
+            FnOnReceiveFut,
+            FnOnClose,
+            FnOnCloseFut,
+        >,
+    ) -> Self {
+        let handler = HandlerWrapperWebSocket::new(config).set_handler(handler);
+        self.ws_handler_append(handler);
+        self
+    }
+}
