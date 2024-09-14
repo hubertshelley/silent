fcdd78cc52c2f70cbe752a40f2f47c5eb8cd9297
hubertshelley <hubertshelley@163.com>
2024-09-14 14:37:48 +0800

    feat: 添加超时中间件及相关示例代码

--- silent/src/middleware/middlewares/mod.rs
+++ silent/src/middleware/middlewares/mod.rs
@@ -1,5 +1,9 @@
 mod cors;
 mod exception_handler;
+mod request_time_logger;
+mod timeout;
 
 pub use cors::{Cors, CorsType};
 pub use exception_handler::ExceptionHandler;
+pub use request_time_logger::RequestTimeLogger;
+pub use timeout::Timeout;
