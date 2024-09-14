eb428439ea21ea4bd668338492ca6d3bdd7eb486
hubertshelley <hubertshelley@163.com>
2024-09-14 13:46:46 +0800

    feat: 调整prelude

--- silent/src/lib.rs
+++ silent/src/lib.rs
@@ -7,6 +7,7 @@ mod grpc;
 mod handler;
 mod log;
 pub mod middleware;
+pub mod prelude;
 mod route;
 #[cfg(feature = "scheduler")]
 mod scheduler;
@@ -29,10 +30,7 @@ mod ws;
 #[allow(clippy::single_component_path_imports)]
 use multer;
 
-pub use crate::configs::Configs;
-#[cfg(feature = "cookie")]
-pub use crate::core::cookie::CookieExt;
-pub use crate::core::{next::Next, request::Request, response::Response};
+pub use crate::core::{request::Request, response::Response};
 #[cfg(feature = "grpc")]
 pub use crate::grpc::{GrpcHandler, GrpcRegister};
 pub use crate::middleware::{middlewares, MiddleWareHandler};
@@ -44,50 +42,3 @@ pub use headers;
 pub use hyper::{header, Method, StatusCode};
 #[cfg(feature = "scheduler")]
 pub use scheduler::{ProcessTime, Scheduler, SchedulerExt, Task};
-
-pub mod prelude {
-    pub use crate::configs::Configs;
-    #[cfg(feature = "cookie")]
-    pub use crate::core::cookie::CookieExt;
-    #[cfg(feature = "multipart")]
-    pub use crate::core::form::{FilePart, FormData};
-    pub use crate::core::{
-        next::Next, path_param::PathParam, req_body::ReqBody, request::Request, res_body::full,
-        res_body::stream_body, res_body::ResBody, response::Response,
-    };
-    pub use crate::error::{SilentError, SilentResult as Result};
-    #[cfg(feature = "grpc")]
-    pub use crate::grpc::{GrpcHandler, GrpcRegister};
-    #[cfg(feature = "static")]
-    pub use crate::handler::static_handler;
-    pub use crate::handler::Handler;
-    pub use crate::handler::HandlerWrapper;
-    pub use crate::log::*;
-    pub use crate::middleware::MiddleWareHandler;
-    #[cfg(feature = "upgrade")]
-    pub use crate::route::handler_append::WSHandlerAppend;
-    pub use crate::route::handler_append::{HandlerAppend, HandlerGetter};
-    pub use crate::route::{RootRoute, Route, RouteService, RouterAdapt};
-    #[cfg(feature = "scheduler")]
-    pub use crate::scheduler::{SchedulerExt, Task};
-    #[cfg(feature = "security")]
-    pub use crate::security::{argon2, pbkdf2};
-    #[cfg(feature = "server")]
-    pub use crate::service::Server;
-    #[cfg(feature = "sse")]
-    pub use crate::sse::{sse_reply, SSEEvent};
-    #[cfg(feature = "template")]
-    pub use crate::templates::*;
-    #[cfg(feature = "upgrade")]
-    pub use crate::ws::{
-        FnOnClose, FnOnConnect, FnOnNoneResultFut, FnOnReceive, FnOnSend, FnOnSendFut,
-    };
-    #[cfg(feature = "upgrade")]
-    pub use crate::ws::{Message, WebSocket, WebSocketHandler, WebSocketParts};
-    #[cfg(feature = "session")]
-    pub use async_session::{Session, SessionStore};
-    #[cfg(feature = "cookie")]
-    pub use cookie::{time as CookieTime, Cookie};
-    pub use headers;
-    pub use hyper::{header, upgrade, Method, StatusCode};
-}
---
+++ silent/src/prelude.rs
@@ -0,0 +1,44 @@
+pub use crate::configs::Configs;
+#[cfg(feature = "cookie")]
+pub use crate::core::cookie::CookieExt;
+#[cfg(feature = "multipart")]
+pub use crate::core::form::{FilePart, FormData};
+pub use crate::core::{
+    next::Next, path_param::PathParam, req_body::ReqBody, request::Request, res_body::full,
+    res_body::stream_body, res_body::ResBody, response::Response,
+};
+pub use crate::error::{SilentError, SilentResult as Result};
+#[cfg(feature = "grpc")]
+pub use crate::grpc::{GrpcHandler, GrpcRegister};
+#[cfg(feature = "static")]
+pub use crate::handler::static_handler;
+pub use crate::handler::Handler;
+pub use crate::handler::HandlerWrapper;
+pub use crate::log::*;
+pub use crate::middleware::MiddleWareHandler;
+#[cfg(feature = "upgrade")]
+pub use crate::route::handler_append::WSHandlerAppend;
+pub use crate::route::handler_append::{HandlerAppend, HandlerGetter};
+pub use crate::route::{RootRoute, Route, RouteService, RouterAdapt};
+#[cfg(feature = "scheduler")]
+pub use crate::scheduler::{SchedulerExt, Task};
+#[cfg(feature = "security")]
+pub use crate::security::{argon2, pbkdf2};
+#[cfg(feature = "server")]
+pub use crate::service::Server;
+#[cfg(feature = "sse")]
+pub use crate::sse::{sse_reply, SSEEvent};
+#[cfg(feature = "template")]
+pub use crate::templates::*;
+#[cfg(feature = "upgrade")]
+pub use crate::ws::{
+    FnOnClose, FnOnConnect, FnOnNoneResultFut, FnOnReceive, FnOnSend, FnOnSendFut,
+};
+#[cfg(feature = "upgrade")]
+pub use crate::ws::{Message, WebSocket, WebSocketHandler, WebSocketParts};
+#[cfg(feature = "session")]
+pub use async_session::{Session, SessionStore};
+#[cfg(feature = "cookie")]
+pub use cookie::{time as CookieTime, Cookie};
+pub use headers;
+pub use hyper::{header, upgrade, Method, StatusCode};
